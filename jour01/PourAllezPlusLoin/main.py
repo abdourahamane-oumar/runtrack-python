8a160b2e1f87500fd19594dbd32c2767b2674a5d
abdourahamane-oumar <145970624+abdourahamane-oumar@users.noreply.github.com>
2023-11-21 20:02:43 +0100

    Delete jour01 directory

--- jour01/PourAllezPlusLoin/main.py
+++
@@ -1,8 +0,0 @@
-# Saisir la chaîne de caractères depuis l'utilisateur
-chaine = input("Entrez une chaîne de caractères : ")
-
-# Vérifier si la chaîne contient le caractère 'e'
-if 'e' in chaine:
-    print("La chaîne contient le caractère 'e'.")
-else:
-    print("La chaîne ne contient pas le caractère 'e'.")
--- jour01/job04/main.py
+++
@@ -1,2 +0,0 @@
-for lettre in range(ord('z'), ord('a') - 1, -1):
-    print(chr(lettre), end=' ')
